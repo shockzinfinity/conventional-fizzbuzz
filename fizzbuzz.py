015716eff87655953b87570e6a2dd628f1d77718
Jun Yu (M1) <shockz@me.com>
2023-09-15 15:17:15 +0900

    feat: Print fizzbuzz
    Print fizzbuzz if i is multiple of 15.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,7 +8,9 @@ def do_fizzbuzz():
     나머지는 숫자 그대로를 출력합니다.
     """
     for i in range(1, 20 + 1):
-        if i % 3 == 0:
+        if i % 15 == 0:
+            print('fizzbuzz')
+        elif i % 3 == 0:
             print('fizz')
         elif i % 5 == 0:
             print('buzz')

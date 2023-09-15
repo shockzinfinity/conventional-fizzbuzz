8ef4fdf9bbbc88544c4350e10b26ad1525371de6
Jun Yu (M1) <shockz@me.com>
2023-09-15 15:27:54 +0900

    refactor: Do fizzbuzz with 1 if Statement
    Using logical op operator.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -8,12 +8,8 @@ def do_fizzbuzz(num:int):
     나머지는 숫자 그대로를 출력합니다.
     """
     for i in range(1, num + 1):
-        if i % 15 == 0:
-            print('fizzbuzz')
-        elif i % 3 == 0:
-            print('fizz')
-        elif i % 5 == 0:
-            print('buzz')
+        if i % 3 == 0 or i % 5 == 0:
+            print('fizz' * (i % 3 == 0)+'buzz' * (i % 5 == 0))
         else:
             print(i)
 
